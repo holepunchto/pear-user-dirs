ab3ab583f6b15eb469cf5dee0b993a68e2020bfb
ruy-dan <ruy-dan@protonmail.com>
2024-06-19 23:07:56 +0200

    iteration on user dir path handling and tests

--- test.js
+++ test.js
@@ -1,86 +1,83 @@
 const test = require('brittle')
 const getUserDirs = require('./')
-const getUserDirsSync = require('./').sync
-const { isWindows, isLinux, isMac } = require('which-runtime')
+const { isWindows, isMac } = require('which-runtime')
+const os = require('os')
 const path = require('path')
 const { spawnSync } = require('child_process')
 
-test('downloads should return a string path', function (t) {
+const HOME_DIR = os.homedir()
+const DEFAULT_PATH = path.join(HOME_DIR, 'Downloads')
+
+test('getUserDirs should return a string path', function (t) {
   t.plan(2)
-  const { downloads: dir } = getUserDirsSync()
+  const { downloads: dir } = getUserDirs({ sync: true })
   t.is(typeof dir, 'string')
   t.ok(dir.length > 0)
 })
 
-test('downloads should return a file URL if requested', function (t) {
+test('getUserDirs should return a file URL if requested', function (t) {
   t.plan(2)
-  const { downloads: downloadsURL } = getUserDirsSync({ asFileURL: true })
-  t.is(typeof downloadsURL, 'string')
-  t.ok(downloadsURL.startsWith('file://'))
+  const { downloads: link } = getUserDirs({ sync: true, asFileLinks: true })
+  t.is(typeof link, 'string')
+  t.ok(link.startsWith('file://'))
 })
 
-test('downloads should return a string path asynchronously', async function (t) {
+test('getUserDirs should return a string path asynchronously', async function (t) {
   t.plan(2)
   const { downloads: dir } = await getUserDirs()
   t.is(typeof dir, 'string')
   t.ok(dir.length > 0)
 })
 
-test('downloads should return a file URL asynchronously if requested', async function (t) {
+test('getUserDirs should return a file URL asynchronously if requested', async function (t) {
   t.plan(2)
-  const { downloads: downloadsURL } = await getUserDirs({ asFileURL: true })
-  t.is(typeof downloadsURL, 'string')
-  t.ok(downloadsURL.startsWith('file://'))
+  const { downloads: link } = await getUserDirs({ asFileLinks: true })
+  t.is(typeof link, 'string')
+  t.ok(link.startsWith('file://'))
 })
 
-test('downloads should return the correct path for each OS', function (t) {
+test('getUserDirs should return the correct path for each OS', function (t) {
   t.plan(1)
-  const { downloads: dir } = getUserDirsSync()
-  const homeDir = require('os').homedir()
 
-  if (isWindows) {
-    t.ok(dir.includes('Downloads'))
-  } else if (isMac) {
-    t.is(dir, path.join(homeDir, 'Downloads'))
-  } else if (isLinux) {
-    const defaultPath = path.join(homeDir, 'Downloads')
-    const xdgPath = () => {
-      try {
-        const result = spawnSync('xdg-user-dir', ['DOWNLOAD'], { stdio: 'pipe' })
-        return result.stdout.toString().trim()
-      } catch (e) {
-        return defaultPath
+  const expected = isWindows
+    ? dir => dir.includes('Downloads')
+    : isMac
+      ? dir => dir.includes('Downloads')
+      : dir => {
+        const xdgPath = () => {
+          try {
+            const result = spawnSync('xdg-user-dir', ['DOWNLOAD'], { stdio: 'pipe' })
+            return result.stdout.toString().trim()
+          } catch (e) {
+            return null
+          }
+        }
+        return dir === DEFAULT_PATH || dir === xdgPath() || dir === '/tmp'
       }
-    }
-    const xdg = xdgPath()
-    t.ok(dir === defaultPath || dir === xdg)
-  } else {
-    t.fail('Unknown OS')
-  }
+
+  const { downloads: dir } = getUserDirs({ sync: true })
+  t.ok(expected(dir))
 })
 
-test('downloads should return the correct path for each OS asynchronously', async function (t) {
+test('getUserDirs should return the correct path for each OS asynchronously', async function (t) {
   t.plan(1)
+
+  const expected = isWindows
+    ? dir => dir.includes('Downloads')
+    : isMac
+      ? dir => dir.includes('Downloads')
+      : dir => {
+        const xdgPath = () => {
+          try {
+            const result = spawnSync('xdg-user-dir', ['DOWNLOAD'], { stdio: 'pipe' })
+            return result.stdout.toString().trim()
+          } catch (e) {
+            return null
+          }
+        }
+        return dir === DEFAULT_PATH || dir === xdgPath() || dir === '/tmp'
+      }
+
   const { downloads: dir } = await getUserDirs()
-  const homeDir = require('os').homedir()
-
-  if (isWindows) {
-    t.ok(dir.includes('Downloads'))
-  } else if (isMac) {
-    t.is(dir, path.join(homeDir, 'Downloads'))
-  } else if (isLinux) {
-    const defaultPath = path.join(homeDir, 'Downloads')
-    const xdgPath = () => {
-      try {
-        const result = spawnSync('xdg-user-dir', ['DOWNLOAD'], { stdio: 'pipe' })
-        return result.stdout.toString().trim()
-      } catch (e) {
-        return defaultPath
-      }
-    }
-    const xdg = xdgPath()
-    t.ok(dir === defaultPath || dir === xdg)
-  } else {
-    t.fail('Unknown OS')
-  }
+  t.ok(expected(dir))
 })
