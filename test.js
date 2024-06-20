892b3c937c3ddf80527f1c503ef3e8f8c679f34f
ruy-dan <ruy-dan@protonmail.com>
2024-06-20 11:27:46 +0200

    assert path exists in tests

--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 const test = require('brittle')
+const fs = require('fs')
 const getUserDirs = require('./')
 
-test('getUserDirs should return a string path', function (t) {
-  t.plan(2)
+test('getUserDirs should return an existing string path', function (t) {
+  t.plan(3)
   const { downloads: dir } = getUserDirs({ sync: true })
   t.is(typeof dir, 'string')
   t.ok(dir.length > 0)
+  t.ok(fs.existsSync(dir), `Path should exist: ${dir}`)
 })
 
 test('getUserDirs should return a file URL if requested', function (t) {
@@ -16,10 +18,11 @@ test('getUserDirs should return a file URL if requested', function (t) {
 })
 
 test('getUserDirs should return a string path asynchronously', async function (t) {
-  t.plan(2)
+  t.plan(3)
   const { downloads: dir } = await getUserDirs()
   t.is(typeof dir, 'string')
   t.ok(dir.length > 0)
+  t.execution(fs.promises.access(dir), `Path should exist: ${dir}`)
 })
 
 test('getUserDirs should return a file URL asynchronously if requested', async function (t) {
